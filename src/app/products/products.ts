c8501ee5beeacbca7c91467ccdeef80ffd129724
Chetan Yeole <yeolechetank@outlook.com>
2026-06-11 14:00:33 +0530

    add images

--- src/app/products/products.ts
+++ src/app/products/products.ts
@@ -1,15 +1,21 @@
 import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
 import { MatGridListModule } from '@angular/material/grid-list';
 import { MatCardModule } from '@angular/material/card';
+import { NgFor } from '@angular/common';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-products',
   standalone: true,
-  imports: [MatGridListModule, MatCardModule, CommonModule],
+  imports: [NgFor, MatGridListModule, MatCardModule, RouterModule],
   templateUrl: './products.html',
   styleUrl: './products.css',
 })
 export class Products {
-
+products = [
+  { name: 'Pain Relief Tablets', price: '₹120', image: 'assets/images/product-pain.png' },
+  { name: 'Cough Syrup', price: '₹85', image: 'assets/images/product-cough.png' },
+  { name: 'Immunity Booster', price: '₹150', image: 'assets/images/product-immunity.png' },
+  { name: 'Diabetes Care', price: '₹110', image: 'assets/images/product-diabetes.png' }
+];
 }
