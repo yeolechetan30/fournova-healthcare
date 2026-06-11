5cfbee9dfd1bfcf7d5a64be2fb575f5bb42f95fd
Chetan Yeole <yeolechetank@outlook.com>
2026-06-11 15:17:19 +0530

    fix issus

--- src/app/about/about.ts
+++ src/app/about/about.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-about',
+  standalone: true,
   imports: [],
   templateUrl: './about.html',
   styleUrl: './about.css',
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,14 @@
-import { Routes } from '@angular/router';
+import { Contact } from './contact/contact';
+import { HomeComponent } from './home/home';
 import { Products } from './products/products';
 import { Services } from './services/services';
-import { HomeComponent } from './home/home';
+import { About } from './about/about';
+import { Routes } from '@angular/router';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'products', component: Products },
-  { path: 'services', component: Services }
+  { path: 'services', component: Services },
+  { path: 'about', component: About },
+  { path: 'contact', component: Contact }
 ];
